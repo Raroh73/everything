34366a98da0d23084c677063fabb817d4c979266
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2024-05-30 11:11:55 +0200

    fix: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["templates/**/*.html"],
+  content: ["chat/templates/**/*.html", "core/templates/**/*.html"],
   theme: {
     extend: {},
   },
