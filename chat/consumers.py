26b8d3785ffbf4d3c3ea9d35b32d41d34d519792
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2024-06-02 20:03:52 +0200

    feat: rewrite to sync

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,38 +2,38 @@ import json
 import uuid
 
 import markdown
-from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.generic.websocket import WebsocketConsumer
 from django.conf import settings
 from django.template.loader import render_to_string
-from openai import AsyncOpenAI
+from openai import OpenAI
 
 from . import models
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.openai_client = AsyncOpenAI(
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.openai_client = OpenAI(
             base_url=settings.OPENAI_URL,
             api_key=settings.OPENAI_KEY,
         )
 
         self.chat = models.Chat()
-        await self.chat.asave()
+        self.chat.save()
 
-        await self.accept()
+        self.accept()
 
-    async def receive(self, text_data):
-        self.chat.arefresh_from_db()
+    def receive(self, text_data):
+        self.chat.refresh_from_db()
 
         message_text = json.loads(text_data)["message"]
 
         user_message_html = render_to_string(
             "message.html", {"message_text": message_text}
         )
-        await self.send(text_data=user_message_html)
+        self.send(text_data=user_message_html)
 
         self.chat.messages.append({"role": "user", "content": message_text})
-        await self.chat.asave()
+        self.chat.save()
 
         message_id = f"message-{uuid.uuid4().hex}"
 
@@ -45,23 +45,23 @@ class ChatConsumer(AsyncWebsocketConsumer):
             },
         )
 
-        await self.send(text_data=system_message_html)
+        self.send(text_data=system_message_html)
 
-        openai_response = await self.openai_client.chat.completions.create(
+        openai_response = self.openai_client.chat.completions.create(
             messages=self.chat.messages,
             stream=True,
             model=settings.MODEL,
         )
 
         message = ""
-        async for chunk in openai_response:
+        for chunk in openai_response:
             message_chunk = chunk.choices[0].delta.content
             if message_chunk is not None:
                 message += message_chunk
-                await self.send(
+                self.send(
                     text_data=f'<div id="{message_id}" hx-swap-oob="innerHTML">{markdown.markdown(message, extensions=["codehilite", "fenced_code"])}</div>'
                 )
 
         self.chat.messages.append({"role": "assistant", "content": message})
-        await self.chat.asave()
+        self.chat.save()
         print(self.chat.messages)
