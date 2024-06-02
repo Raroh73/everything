f6463bb82e62186e3dcd6ee0e52db3c9abbe583e
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2024-06-02 15:38:17 +0200

    build: add simple tests

---
+++ chat/tests.py
@@ -0,0 +1,13 @@
+import unittest
+
+from django.test import Client
+
+
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def get_chat(self):
+        response = self.client.get("/chat")
+
+        self.assertEqual(response.status_code, 200)
