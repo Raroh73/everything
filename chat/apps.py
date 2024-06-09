338ebaaebbc6c20165998c40152ae32e77d00aa0
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2024-06-09 09:47:25 +0200

    feat: add model pull on app start

--- chat/apps.py
+++ chat/apps.py
@@ -1,5 +1,10 @@
+import ollama
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class ChatConfig(AppConfig):
     name = "chat"
+
+    def ready(self):
+        ollama.pull(settings.MODEL)
